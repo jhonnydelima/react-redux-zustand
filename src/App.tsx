3133b70bce608648ccacaf26a9b4b9073bdd0e0b
Jhonny Lima <lima.jhonny95@gmail.com>
2025-08-22 02:28:25 -0300

    feat: remove to-do components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { Provider as ReduxProvider } from "react-redux";
-import { AddTodo } from "./components/AddTodo";
-import { TodoList } from "./components/TodoList";
 import { store } from "./store";
 
 export function App() {
   return (
     <ReduxProvider store={store}>
-      <TodoList />
-      <AddTodo />
+      <h1>Hello, Redux!</h1>
     </ReduxProvider>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,23 +1,9 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import { useSelector, type TypedUseSelectorHook } from "react-redux";
 
-const todoSlice = createSlice({
-  name: "todo",
-  initialState: ["Study Redux", "Learn Zustand", "Dormir"],
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo);
-    },
-  },
-});
-
 export const store = configureStore({
-  reducer: {
-    todo: todoSlice.reducer,
-  },
+  reducer: {},
 });
-
-export const { add } = todoSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
